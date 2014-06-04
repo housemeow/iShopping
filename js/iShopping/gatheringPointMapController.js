bd3bbeddb6316c35d0807fa755f4c70108a04e30
Kai <housemeow@gmail.com>
2014-06-05 01:05:39 +0800

    map can search place and put place

--- js/iShopping/gatheringPointMapController.js
+++ js/iShopping/gatheringPointMapController.js
@@ -36,6 +36,7 @@ app.controller('GatheringPointMapController', function($scope, $stateParams,
 
 	var initMap = function(geoposition) {
 		console.log(geoposition);
+
 		var origin = new google.maps.LatLng(geoposition.coords.latitude,
 				geoposition.coords.longitude);
 		var mapOptions = {
@@ -57,6 +58,32 @@ app.controller('GatheringPointMapController', function($scope, $stateParams,
 			}
 		});
 		marker.setMap(map);
+
+		// 建立搜尋列的UI
+		var input = (document.getElementById('pac-input'));
+		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+		var searchBox = new google.maps.places.SearchBox((input));
+		// 監聽搜尋列搜尋出來的清單，點選項目之後可以在searchBox.getPlaces()取得符合的項目
+		google.maps.event.addListener(searchBox, 'places_changed', function() {
+			var places = searchBox.getPlaces();
+			// 取得第一個地標
+			var place = places[0];
+			origin = place.geometry.location;
+			marker.setPosition(origin);
+			// 設定畫面的解析度
+			var bounds = new google.maps.LatLngBounds(place.geometry.location);
+			map.fitBounds(bounds);
+		});
+
+		// Bias the SearchBox results towards places that are within the bounds
+		// of the
+		// current map's viewport.
+		// 
+		google.maps.event.addListener(map, 'bounds_changed', function() {
+			var bounds = map.getBounds();
+			searchBox.setBounds(bounds);
+		});
+
 		google.maps.event.addListener(map, 'click', function(event) {
 			placeMarker(event.latLng);
 		});
@@ -68,56 +95,9 @@ app.controller('GatheringPointMapController', function($scope, $stateParams,
 	};
 
 	$scope.init = function() {
-		// Geolocation.getCurrentPosition(initMap);
-		// $scope.state = $stateParams.state;
-		// $scope.eid = $stateParams.eid;
-		// $scope.refreshState($scope.state);
-
-		var markers = [];
-		var map = new google.maps.Map(document.getElementById('map-canvas'), {
-			mapTypeId : google.maps.MapTypeId.ROADMAP,
-			disableDefaultUI : true
-		});
-
-		var defaultBounds = new google.maps.LatLngBounds(
-				new google.maps.LatLng(-33.8902, 151.1759),
-				new google.maps.LatLng(-33.8474, 151.2631));
-		map.fitBounds(defaultBounds);
-
-		// Create the search box and link it to the UI element.
-		var input = (document.getElementById('pac-input'));
-		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-		var searchBox = new google.maps.places.SearchBox((input));
-
-		// Listen for the event fired when the user selects an item from the
-		// pick list. Retrieve the matching places for that item.
-		google.maps.event.addListener(searchBox, 'places_changed', function() {
-			var places = searchBox.getPlaces();
-
-			// For each place, get the icon, place name, and location.
-			var anchor = new google.maps.Point(30, 65);
-			var place = places[0];
-			var origin = place.geometry.location;
-			var marker = new MarkerWithLabel({
-				position : origin,
-				labelContent : "集合點",
-				labelAnchor : anchor,
-				labelClass : "labels",
-				labelStyle : {
-					opacity : 0.75
-				}
-			});
-			marker.setMap(map);
-			var bounds = new google.maps.LatLngBounds(place.geometry.location);
-			map.fitBounds(bounds);
-		});
-
-		// Bias the SearchBox results towards places that are within the bounds
-		// of the
-		// current map's viewport.
-		google.maps.event.addListener(map, 'bounds_changed', function() {
-			var bounds = map.getBounds();
-			searchBox.setBounds(bounds);
-		});
+		Geolocation.getCurrentPosition(initMap);
+		$scope.state = $stateParams.state;
+		$scope.eid = $stateParams.eid;
+		$scope.refreshState($scope.state);
 	};
 });
