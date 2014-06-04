90ce856bfbe819103dc884e7d0e29f024050e7fe
Jay-Liao <j9950314@gmail.com>
2014-06-04 17:15:44 +0800

    all event page switch ok

--- js/iShopping/eventController.js
+++ js/iShopping/eventController.js
@@ -4,11 +4,14 @@ app.controller('EventController', function($scope, ChatManager, $stateParams, Fr
 				type: 'button-positive'
 		};
 		if($scope.state=="VIEW"){
+			$scope.title = "活動詳細資料";
 			buttonCSS.tap = function(){
-				// prepare to edit state
+				console.log("now is view");
+				$state.go('event', {state:'EDIT'});
 			};
 			buttonCSS.content = "<i class='icon ion-edit'></i>";
 		}else if($scope.state=="CREATE"){
+			$scope.title = "建立活動";
 			buttonCSS.tap = function(){
 				// go to event list
 				$state.go('tab.eventList');
@@ -16,13 +19,16 @@ app.controller('EventController', function($scope, ChatManager, $stateParams, Fr
 			buttonCSS.content = "<i class='icon ion-checkmark'></i>";
 			
 		}else if($scope.state=="EDIT"){
+			$scope.title = "編輯活動";
 			buttonCSS.tap = function(){
-				// back to view state
+				console.log("now is edit");
+				$state.go('event', {state:'VIEW'});
 			};
 			buttonCSS.content = "<i class='icon ion-checkmark'></i>";
 		}
 		console.log($scope.state);
-		$scope.eventButton = [buttonCSS];	
+		$scope.eventButton = [buttonCSS];
+		$scope.apply();
 	}
 	
 	$scope.event = {};
