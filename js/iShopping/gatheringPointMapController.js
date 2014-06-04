b094dfb4d6e1315fc2b97fa3cf407aa7742eb0c3
Kai <housemeow@gmail.com>
2014-06-05 00:55:42 +0800

    map can search place

--- js/iShopping/gatheringPointMapController.js
+++ js/iShopping/gatheringPointMapController.js
@@ -63,27 +63,61 @@ app.controller('GatheringPointMapController', function($scope, $stateParams,
 
 		function placeMarker(location) {
 			origin = location;
-			// if (marker == null) {
-			// marker = new MarkerWithLabel({
-			// position : origin,
-			// labelContent : "集合點",
-			// labelAnchor : anchor,
-			// labelClass : "labels",
-			// labelStyle : {
-			// opacity : 0.75
-			// }
-			// });
-			// marker.setMap(map);
-			// } else {
 			marker.setPosition(location);
-			// }
 		}
 	};
 
 	$scope.init = function() {
-		Geolocation.getCurrentPosition(initMap);
-		$scope.state = $stateParams.state;
-		$scope.eid = $stateParams.eid;
-		$scope.refreshState($scope.state);
+		// Geolocation.getCurrentPosition(initMap);
+		// $scope.state = $stateParams.state;
+		// $scope.eid = $stateParams.eid;
+		// $scope.refreshState($scope.state);
+
+		var markers = [];
+		var map = new google.maps.Map(document.getElementById('map-canvas'), {
+			mapTypeId : google.maps.MapTypeId.ROADMAP,
+			disableDefaultUI : true
+		});
+
+		var defaultBounds = new google.maps.LatLngBounds(
+				new google.maps.LatLng(-33.8902, 151.1759),
+				new google.maps.LatLng(-33.8474, 151.2631));
+		map.fitBounds(defaultBounds);
+
+		// Create the search box and link it to the UI element.
+		var input = (document.getElementById('pac-input'));
+		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+		var searchBox = new google.maps.places.SearchBox((input));
+
+		// Listen for the event fired when the user selects an item from the
+		// pick list. Retrieve the matching places for that item.
+		google.maps.event.addListener(searchBox, 'places_changed', function() {
+			var places = searchBox.getPlaces();
+
+			// For each place, get the icon, place name, and location.
+			var anchor = new google.maps.Point(30, 65);
+			var place = places[0];
+			var origin = place.geometry.location;
+			var marker = new MarkerWithLabel({
+				position : origin,
+				labelContent : "集合點",
+				labelAnchor : anchor,
+				labelClass : "labels",
+				labelStyle : {
+					opacity : 0.75
+				}
+			});
+			marker.setMap(map);
+			var bounds = new google.maps.LatLngBounds(place.geometry.location);
+			map.fitBounds(bounds);
+		});
+
+		// Bias the SearchBox results towards places that are within the bounds
+		// of the
+		// current map's viewport.
+		google.maps.event.addListener(map, 'bounds_changed', function() {
+			var bounds = map.getBounds();
+			searchBox.setBounds(bounds);
+		});
 	};
 });
